0118f272be708b6c4390bb214d600ef7ecda4c49
Tech Plus <76077831+techpluscraft@users.noreply.github.com>
2021-09-22 00:46:59 +0000

    Made bot work

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import nextcord
 from nextcord.ext import commands, tasks
+
+intents=nextcord.Intents.all()
+client = commands.Bot(command_prefix = 'h!',intents=intents)
+
+@client.event
+async def on_ready():
+	print('Bot is Online')
+
+client.run("TOKEN")
