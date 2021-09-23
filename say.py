6c437fda530e807785acd4bc52ff0a3557fd3f35
whenyoudontknowhowtocode <81732383+alexyy802@users.noreply.github.com>
2021-09-23 14:32:13 +0700

    Create say.py

---
+++ say.py
@@ -0,0 +1,21 @@
+import nextcord
+from nextcord.ext import commands
+
+class say(commands.Cog):
+    def __init__(self, client):
+    self.client = client
+    
+    
+    @commands.command()
+    async def say(self, ctx, arg=None):
+      if arg == None:
+        await ctx.reply('Give me a word to say!')
+      else:
+        e=nextcord.Embed(description=arg)
+        e.set_footer(text=f"Requested by {ctx.author.name}")
+        await ctx.send(embed=e)
+        
+        
+        
+def setup(client):
+  client.add_cog(say(client))
