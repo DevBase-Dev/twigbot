5fab9c1e4bac74bb7929751ecf9304ff02e23b41
Tech Plus <76077831+techpluscraft@users.noreply.github.com>
2021-09-27 14:00:07 +0000

    fixed say command

--- cogs/say.py
+++ cogs/say.py
@@ -7,11 +7,11 @@ class say(commands.Cog):
     
     
     @commands.command()
-    async def say(self, ctx, arg=None):
-      if arg == None:
+    async def say(self, ctx, *,message=None):
+      if message == None:
         await ctx.reply('Give me a word to say!')
       else:
-        e=nextcord.Embed(description=arg)
+        e=nextcord.Embed(description=message)
         e.set_footer(text=f"Requested by {ctx.author.name}")
         await ctx.send(embed=e)
         
